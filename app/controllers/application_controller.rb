78796e8df7d679e88fc15bff8e734809a0df92f5
LeraRad <exlab7@gmail.com>
2023-04-19 19:29:19 +0200

    Added guest user for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
+
 end
 
