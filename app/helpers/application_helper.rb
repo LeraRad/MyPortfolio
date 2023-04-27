e677d5922c767f934b5bd4e44ae60bf2038918c5
LeraRad <exlab7@gmail.com>
2023-04-27 20:50:26 +0200

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User) 
-      button_to "Logout", destroy_user_session_path, method: :delete 
-    else 
-       (button_to "Register", new_user_registration_path) +
+    if current_user.is_a?(GuestUser)
+      (button_to "Register", new_user_registration_path) +
        "<br>".html_safe +  
-       (button_to "Login", new_user_session_path) 
+      (button_to "Login", new_user_session_path)  
+    else 
+      button_to "Logout", destroy_user_session_path, method: :delete  
     end 
   end
 
